2b24f3af6b4dca62dc729d51bfa37784b76c935a
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-05-10 12:56:48 +0530

    Add app file

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import movies from "./routs/movies.js";
+import createError from "http-errors";
+
+const app = express();
+
+//create connection
+
+app.use(express.json());
+
+app.use("/", movies);
+
+// handle 404 errors
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json(err.messageObj || { status: "Error" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("server listining to port", PORT);
+});
